4c25f72c21ea3d9471ac9a366d3b416f91efad1c
João Lucas Mazetti <jlmazetti03@hotmail.com>
2025-06-20 22:17:14 -0300

    feat: adiciona variaveis sobre conta bancaria

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,11 @@
+public class ContaTerminal {
+    private String agencia;
+    private String nomeCliente;
+    private int numero;
+    private double saldo;
+
+    public static void main(String[] args) {
+        
+    }
+
+}
