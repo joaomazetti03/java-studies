a3650733673289c65c53360e31a87dc5dc98a920
João Lucas Mazetti <jlmazetti03@hotmail.com>
2025-06-20 23:16:39 -0300

    feat: adiciona atributos sem modificador de acesso e scanner para coletar dados no terminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,11 +1,28 @@
-public class ContaTerminal {
-    private String agencia;
-    private String nomeCliente;
-    private int numero;
-    private double saldo;
+import java.util.Scanner;
 
+public class ContaTerminal {
     public static void main(String[] args) {
-        
+        //atributos
+        String agencia;
+        String nomeCliente;
+        int numero;
+        double saldo;
+
+        //coletar dados do terminal
+        Scanner input = new Scanner(System.in);
+        System.out.print("Digite o nome do Cliente: ");
+        nomeCliente = input.nextLine();
+
+        System.out.print("Digite a agência do Cliente: ");
+        agencia = input.nextLine();
+
+        System.out.print("Digite o numero do Cliente: ");
+        numero = input.nextInt();
+
+        System.out.print("Digite o saldo do Cliente: ");
+        saldo = input.nextDouble();
+
+        System.out.print(nomeCliente);
     }
 
 }
